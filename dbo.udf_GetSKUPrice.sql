ac13d4dea328d4d21d281af5dc14eba7182a419e
Ravil Mamyshev <ravilmamyshev@yandex.ru>
2023-08-13 23:53:50 +0400

    Added dbo.udf_GetSKUPrice.sql

---
+++ dbo.udf_GetSKUPrice.sql
@@ -0,0 +1,16 @@
+﻿IF OBJECT_ID('dbo.GetSKUPrice') IS NOT NULL
+	DROP FUNCTION dbo.GetSKUPrice;
+GO
+CREATE FUNCTION dbo.GetSKUPrice
+(
+	@ID_SKU AS INT
+)
+RETURNS DECIMAL(18, 2)
+AS
+	BEGIN
+		DECLARE @sumValue INT, @sumQuantity INT
+		SELECT @sumValue = SUM(Value), @sumQuantity = SUM(Quantity) 
+		FROM dbo.Basket
+		WHERE ID_SKU = @ID_SKU
+		RETURN CAST(@sumValue * 1.00 / @sumQuantity AS DECIMAL(18, 2))
+	END;
